3cb20b059300865e4652e79e8bf0d9f7af3ef91d
sauravBalodi1 <sauravbalodi1234@gmail.com>
2021-03-09 01:54:04 +0530

    navbar Css completed

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css';
+import Header from "../src/Header";
+function App() {
+  return (
+    <div>
+     
+      {/* Header */}
+      <Header />
+      {/* home */}
+    </div>
+    
+  )
+  
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBrDZr_DaTkB-G3Hf2VMFGnOlbB67jglAw",
+    authDomain: "project-9a751.firebaseapp.com",
+    projectId: "project-9a751",
+    storageBucket: "project-9a751.appspot.com",
+    messagingSenderId: "951294210549",
+    appId: "1:951294210549:web:fc7be174e6997c0737f0bc",
+    measurementId: "G-PQ9X6YG3LG"
+  };
